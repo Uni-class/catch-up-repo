ba494331e4a780b001a9deef787d0fa2009f5342
nayounsang <nayounsang722@gmail.com>
2024-07-10 18:30:43 +0900

    chore: edit tsconfig to use emotion in ts emotion을 typescript nextjs 환경에서 사용하기 위해 jsxImportSource 옵션을 설정했습니다.

---
+++ packages/front-end/style/reset-style.tsx
@@ -0,0 +1,10 @@
+import { css, Global } from '@emotion/react'
+
+const style = css`
+  /* reset.css 관련 코드 작성 */
+`
+
+export default function GlobalStyle() {
+  return <Global styles={style} />
+}
+
