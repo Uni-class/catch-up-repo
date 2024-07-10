76b3f2e485a45dbd09c60f28522187235ba2f5c6
nayounsang <nayounsang722@gmail.com>
2024-07-10 17:57:50 +0900

    chore: unlock ssg option temporary 임시적으로 nextconfig의 ssg 옵션을 해제했습니다. 추후 ssg 방식 라우팅하는 법을 안다면 다시 설정합니다.

--- packages/front-end/next.config.mjs
+++ packages/front-end/next.config.mjs
@@ -3,8 +3,8 @@ const nextConfig = {
   experimental: {
     externalDir: true,
   },
-  output: "export",
-  trailingSlash: true,
+  // output: "export",
+  // trailingSlash: true,
   images: { unoptimized: true },
 };
 
