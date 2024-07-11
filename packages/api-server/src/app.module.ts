7e349c117b5accb69127b418e95e74f79f256806
1000chw <1000chw@gmail.com>
2024-07-11 14:28:43 +0900

    feat: typeORM setting typeORM global로 연결

--- packages/api-server/src/app.module.ts
+++ packages/api-server/src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmModuleOptions } from './configs/typeorm.config';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot(typeOrmModuleOptions),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
