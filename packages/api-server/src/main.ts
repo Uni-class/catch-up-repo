43fe5df64ff4bbbc408ec64b5aa4b9bd6452574a
1000chw <1000chw@gmail.com>
2024-07-11 15:12:34 +0900

    feat: Swagger api doc setting Swagger 설정

--- packages/api-server/src/main.ts
+++ packages/api-server/src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as process from 'node:process';
 
 declare const module: any;
 
@@ -8,7 +10,16 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.use(helmet());
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Catch-UP API Documentation')
+    .setDescription('Catch-UP API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT);
 
   if (module.hot) {
     module.hot.accept();
