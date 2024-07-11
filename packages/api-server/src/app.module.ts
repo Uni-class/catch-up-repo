f89d4fe5b63c2931329f0433fb41b877c4108650
1000chw <1000chw@gmail.com>
2024-07-11 17:17:47 +0900

    feat: Append users.module to app.module

--- packages/api-server/src/app.module.ts
+++ packages/api-server/src/app.module.ts
@@ -5,12 +5,14 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmModuleOptions } from './configs/typeorm.config';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot(typeOrmModuleOptions),
     AuthModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
