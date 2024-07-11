fb82a668170a49996a618fa89df58404ded3e008
1000chw <1000chw@gmail.com>
2024-07-11 16:01:38 +0900

    chore: swagger version

--- packages/api-server/src/main.ts
+++ packages/api-server/src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
   const config = new DocumentBuilder()
     .setTitle('Catch-UP API Documentation')
     .setDescription('Catch-UP API description')
-    .setVersion('1.0')
+    .setVersion('1.0.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
