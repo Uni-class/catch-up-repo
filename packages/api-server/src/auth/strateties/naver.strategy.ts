99e9ae85e785f5c98b0c893561488b3e7c1ae272
1000chw <1000chw@gmail.com>
2024-07-11 17:15:56 +0900

    feat: Make naver auth strategy, guard 네이버 Auth Strategy, guard 생성

---
+++ packages/api-server/src/auth/strateties/naver.strategy.ts
@@ -0,0 +1,20 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, Profile } from 'passport-naver-v2';
+import { AuthService } from '../auth.service';
+import * as process from 'node:process';
+export class NaverStrategy extends PassportStrategy(Strategy, 'naver') {
+  constructor(private authService: AuthService) {
+    super({
+      clientID: process.env.NAVER_CLIENT_ID,
+      clientSecret: process.env.NAVER_CLIENT_SECRET,
+      callbackURL: process.env.NAVER_CALLBACK_URL,
+      passReqToCallback: true,
+    });
+  }
+  async validate(
+    req: Request,
+    accessToken: string,
+    refreshToken: string,
+    profile: Profile,
+  ) {}
+}
