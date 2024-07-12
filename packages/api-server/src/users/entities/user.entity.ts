50abbbc892c823ac815a2c3a202201a14ee72d21
1000chw <1000chw@gmail.com>
2024-07-12 16:25:55 +0900

    feat: Make User DTO

--- packages/api-server/src/users/entities/user.entity.ts
+++ packages/api-server/src/users/entities/user.entity.ts
@@ -1,1 +1,72 @@
-export class User {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsDate, IsNumber, IsString } from 'class-validator';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('users')
+export class User {
+  @ApiProperty()
+  @IsNumber()
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ApiProperty()
+  @IsString()
+  @Column()
+  nickname: string;
+
+  @ApiProperty()
+  @IsString()
+  @Column({ nullable: true })
+  username: string;
+
+  @ApiProperty()
+  @IsString()
+  @Column({ nullable: true })
+  email: string;
+
+  @ApiProperty()
+  @IsString()
+  @Column({ nullable: true })
+  profileUrl: string;
+
+  @ApiProperty()
+  @IsString()
+  @Column()
+  provider: string;
+
+  @ApiProperty()
+  @IsNumber()
+  @Column()
+  providerId: number;
+
+  @ApiProperty()
+  @IsString()
+  @Column()
+  refreshToken: string;
+
+  @ApiProperty()
+  @IsString()
+  @Column()
+  status: string;
+
+  @ApiProperty()
+  @IsDate()
+  @Column({ default: Date.now() })
+  createdAt: Date;
+
+  @ApiProperty()
+  @IsDate()
+  @Column({ nullable: true })
+  updatedAt: Date;
+
+  @ApiProperty()
+  @IsDate()
+  @Column({ nullable: true })
+  deletedAt: Date;
+}
