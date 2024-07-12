87747e2d3ad290f7ee8002e3638f6a5da2775c2a
nayounsang <nayounsang722@gmail.com>
2024-07-12 14:45:35 +0900

    feat: config query param naver kakao google naver kakao google에 필요한 query param을 작성

--- packages/front-end/app/login/_components/naver-login.tsx
+++ packages/front-end/app/login/_components/naver-login.tsx
@@ -1,4 +1,3 @@
-import styled from "styled-components";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -7,7 +6,12 @@ export default function NaverLogin() {
     <Link
       href={{
         pathname: "https://nid.naver.com/oauth2.0/authorize",
-        query: { client_id: process.env.NEXT_PUBLIC_NAVER_CLIENT_ID, response_type: 'code' },
+        query: {
+          client_id: process.env.NEXT_PUBLIC_NAVER_CLIENT_ID,
+          response_type: "code",
+          redirect_uri: process.env.NEXT_PUBLIC_NAVER_CALLBACK_URL,
+          state: "test",
+        },
       }}
     >
       <Image src="/login-naver.png" alt="login" width={200} height={50} />
