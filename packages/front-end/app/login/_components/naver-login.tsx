13bfeac63ec4d9b903a4e4ba1175902d5683f8f2
nayounsang <nayounsang722@gmail.com>
2024-07-12 13:33:03 +0900

    feat: naver login api client id 적용 환경변수를 이용해 naver login api 적용

--- packages/front-end/app/login/_components/naver-login.tsx
+++ packages/front-end/app/login/_components/naver-login.tsx
@@ -1,10 +1,16 @@
 import styled from "styled-components";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function NaverLogin() {
   return (
-    <a href="https://nid.naver.com/oauth2.0/authorize">
+    <Link
+      href={{
+        pathname: "https://nid.naver.com/oauth2.0/authorize",
+        query: { client_id: process.env.NEXT_PUBLIC_NAVER_CLIENT_ID },
+      }}
+    >
       <Image src="/login-naver.png" alt="login" width={200} height={50} />
-    </a>
+    </Link>
   );
 }
