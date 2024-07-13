a6abfa215d212df1994936d0f526b6ec0dc10cea
nayounsang <nayounsang722@gmail.com>
2024-07-13 23:31:04 +0900

    feat: migrate theme for panda css panda config를 작성해 기존 소셜로그인버튼 컬러 테마를 마이그레이션

--- packages/front-end/panda.config.ts
+++ packages/front-end/panda.config.ts
@@ -5,14 +5,34 @@ export default defineConfig({
   preflight: true,
 
   // Where to look for your css declarations
-  include: ["./src/components/**/*.{ts,tsx,js,jsx}", "./src/app/**/*.{ts,tsx,js,jsx}"],
-  
+  include: [
+    "./src/components/**/*.{ts,tsx,js,jsx}",
+    "./src/app/**/*.{ts,tsx,js,jsx}",
+  ],
+
   // Files to exclude
   exclude: [],
 
   // Useful for theme customization
   theme: {
-    extend: {},
+    extend: {
+      tokens: {
+        colors: {
+          naver: {
+            green: { value: "#03c75a" },
+          },
+          kakao: {
+            container: { value: "#fee500" },
+            symbol: { value: "#000" },
+            lable: { value: "#000000d8" },
+          },
+          google: {
+            bright: { value: "#fff" },
+            dark: { value: "#4285F4" },
+          },
+        },
+      },
+    },
   },
 
   // The output directory for your css system
