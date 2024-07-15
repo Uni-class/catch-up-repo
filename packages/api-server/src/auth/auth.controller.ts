424e755be57f41b1299d0d64ad9b7fda34a7adc6
1000chw <1000chw@gmail.com>
2024-07-15 17:30:41 +0900

    feat: make access, refresh token 로그인 했을 때, access token, refresh token 생성 access token이 만료됐을 땐 refresh token으로 새로 발급 받음

--- packages/api-server/src/auth/auth.controller.ts
+++ packages/api-server/src/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, UseGuards, Req, Res } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  UseGuards,
+  Req,
+  Res,
+  UnauthorizedException,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { NaverAuthGuard } from './guards/naverauth.guard';
 import { Request, Response } from 'express';
@@ -6,6 +15,8 @@ import { GoogleAuthGuard } from './guards/googleauth.guard';
 import { KakaoAuthGuard } from './guards/kakaoauth.guard';
 import * as process from 'node:process';
 import { UsersService } from '../users/users.service';
+import { User } from '../users/entities/user.entity';
+import { RefreshGuard } from './guards/refresh.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -21,8 +32,14 @@ export class AuthController {
     @Res({ passthrough: true }) res: Response,
   ): Promise<any> {
     try {
-      const user = req.user;
-      return res.redirect(process.env.CLIENT_REDIRECT_URL);
+      const user: User = req.user as User;
+      const accessToken = await this.authService.generateAccessToken(user);
+      const refreshToken = await this.authService.generateRefreshToken(user);
+      await this.userService.update(user.id, { refreshToken });
+      return res
+        .cookie('access_token', accessToken)
+        .cookie('refresh_token', refreshToken)
+        .redirect('http://localhost:3000/');
     } catch (e) {
       console.log(e);
     }
@@ -35,8 +52,14 @@ export class AuthController {
     @Res({ passthrough: true }) res: Response,
   ): Promise<any> {
     try {
-      console.log(req.user);
-      return res.redirect(process.env.CLIENT_REDIRECT_URL);
+      const user: User = req.user as User;
+      const accessToken = await this.authService.generateAccessToken(user);
+      const refreshToken = await this.authService.generateRefreshToken(user);
+      await this.userService.update(user.id, { refreshToken });
+      return res
+        .cookie('access_token', accessToken)
+        .cookie('refresh_token', refreshToken)
+        .redirect('http://localhost:3000/');
     } catch (e) {
       console.log(e);
     }
@@ -49,10 +72,34 @@ export class AuthController {
     @Res({ passthrough: true }) res: Response,
   ): Promise<any> {
     try {
-      console.log(req.user);
-      return res.redirect(process.env.CLIENT_REDIRECT_URL);
+      const user: User = req.user as User;
+
+      const accessToken = await this.authService.generateAccessToken(user);
+      const refreshToken = await this.authService.generateRefreshToken(user);
+      await this.userService.update(user.id, { refreshToken });
+      return res
+        .cookie('access_token', accessToken)
+        .cookie('refresh_token', refreshToken)
+        .redirect('http://localhost:3000/');
     } catch (e) {
       console.log(e);
     }
+  }
+
+  @UseGuards(RefreshGuard)
+  @HttpCode(HttpStatus.CREATED)
+  @Get('token-refresh')
+  async tokenRefresh(@Req() req: Request, @Res() res): Promise<any> {
+    const refreshToken = req.cookies.refresh_token;
+    const user: User = req.user as User;
+    if (user.refreshToken !== refreshToken) {
+      return res
+        .status(HttpStatus.UNAUTHORIZED)
+        .json({ msg: `This refresh token is not user's token` });
+    }
+    const newAccessToken = await this.authService.generateAccessToken(user);
+    return res
+      .cookie('access_token', newAccessToken)
+      .json({ msg: 'Refresh token successfully.' });
   }
 }
