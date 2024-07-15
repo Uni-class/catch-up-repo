da39135f80eefb948e6f5ed5edea28e4eb7ed944
1000chw <1000chw@gmail.com>
2024-07-15 17:22:19 +0900

    feat: Make jwt(access-token) validation cookie에서 jwt 토큰 가져온 후 validation

---
+++ packages/api-server/src/auth/strateties/jwt.strategy.ts
@@ -0,0 +1,33 @@
+import { AuthService } from '../auth.service';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy, VerifiedCallback } from 'passport-jwt';
+import { Request } from 'express';
+import { JwtPayload } from '../jwt.payload';
+import { User } from '../../users/entities/user.entity';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(private authService: AuthService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (req: Request) => {
+          return req?.cookies?.access_token;
+        },
+      ]),
+      ignoreExpiration: false,
+      secretOrKey: process.env.ACCESS_JWT_SECRET,
+    });
+  }
+
+  async validate(payload: JwtPayload, done: VerifiedCallback): Promise<any> {
+    const user: User = await this.authService.tokenValidateUser(payload);
+    if (!user) {
+      return done(
+        new UnauthorizedException({ message: '존재 하지 않는 유저' }),
+        false,
+      );
+    }
+    return done(null, user);
+  }
+}
