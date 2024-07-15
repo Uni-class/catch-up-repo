4a88d1f785aca84a328387f5d18c6dc09f0d2a93
1000chw <1000chw@gmail.com>
2024-07-15 14:29:02 +0900

    feat: Make user validation 유저가 있는지 찾아서 없으면 만들어준다. 그 후 해당 유저 데이터를 반환한다.

--- packages/api-server/src/auth/auth.service.ts
+++ packages/api-server/src/auth/auth.service.ts
@@ -1,6 +1,44 @@
 import { Injectable } from '@nestjs/common';
+import { Profile as KakaoProfile } from 'passport-kakao';
+import { Profile as GoogleProfile } from 'passport-google-oauth20';
+import { Profile as NaverProfile } from 'passport-naver-v2';
+import { UsersService } from '../users/users.service';
+import { UserConverter } from '../users/user.converter';
 
 @Injectable()
 export class AuthService {
-  async validateUser(provider: string, id: string) {}
+  constructor(
+    private readonly userService: UsersService,
+    private userConverter: UserConverter,
+  ) {}
+  async validateGoogleUser(profile: GoogleProfile) {
+    const user = await this.userService.findOne(profile.id, profile.provider);
+    if (!user) {
+      const newUser = await this.userService.create(
+        await this.userConverter.googleUserConverter(profile),
+      );
+      return newUser;
+    }
+    return user;
+  }
+  async validateNaverUser(profile: NaverProfile) {
+    const user = await this.userService.findOne(profile.id, profile.provider);
+    if (!user) {
+      const newUser = await this.userService.create(
+        await this.userConverter.naverUserConverter(profile),
+      );
+      return newUser;
+    }
+    return user;
+  }
+  async validateKakaoUser(profile: KakaoProfile) {
+    const user = await this.userService.findOne(profile.id, profile.provider);
+    if (!user) {
+      const newUser = await this.userService.create(
+        await this.userConverter.kakaoUserConverter(profile),
+      );
+      return newUser;
+    }
+    return user;
+  }
 }
