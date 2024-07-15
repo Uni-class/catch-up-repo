601016c7b39077c78b546baeac6bb3ac7c87e166
1000chw <1000chw@gmail.com>
2024-07-15 17:30:57 +0900

    feat: jwt token 발급 과정

--- packages/api-server/src/auth/auth.service.ts
+++ packages/api-server/src/auth/auth.service.ts
@@ -4,15 +4,24 @@ import { Profile as GoogleProfile } from 'passport-google-oauth20';
 import { Profile as NaverProfile } from 'passport-naver-v2';
 import { UsersService } from '../users/users.service';
 import { UserConverter } from '../users/user.converter';
+import { User } from '../users/entities/user.entity';
+import { JwtService } from '@nestjs/jwt';
+import * as process from 'node:process';
+import { JwtPayload } from './jwt.payload';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly userService: UsersService,
     private userConverter: UserConverter,
+    private jwtService: JwtService,
   ) {}
+
   async validateGoogleUser(profile: GoogleProfile) {
-    const user = await this.userService.findOne(profile.id, profile.provider);
+    const user = await this.userService.findOneByProviderId(
+      profile.id,
+      profile.provider,
+    );
     if (!user) {
       const newUser = await this.userService.create(
         await this.userConverter.googleUserConverter(profile),
@@ -21,8 +30,12 @@ export class AuthService {
     }
     return user;
   }
+
   async validateNaverUser(profile: NaverProfile) {
-    const user = await this.userService.findOne(profile.id, profile.provider);
+    const user = await this.userService.findOneByProviderId(
+      profile.id,
+      profile.provider,
+    );
     if (!user) {
       const newUser = await this.userService.create(
         await this.userConverter.naverUserConverter(profile),
@@ -31,8 +44,12 @@ export class AuthService {
     }
     return user;
   }
+
   async validateKakaoUser(profile: KakaoProfile) {
-    const user = await this.userService.findOne(profile.id, profile.provider);
+    const user = await this.userService.findOneByProviderId(
+      profile.id,
+      profile.provider,
+    );
     if (!user) {
       const newUser = await this.userService.create(
         await this.userConverter.kakaoUserConverter(profile),
@@ -40,5 +57,22 @@ export class AuthService {
       return newUser;
     }
     return user;
+  }
+
+  async generateAccessToken(user: User) {
+    const payload: JwtPayload = { id: user.id };
+    return await this.jwtService.signAsync(payload);
+  }
+
+  async generateRefreshToken(user: User) {
+    const payload = { id: user.id };
+    return await this.jwtService.signAsync(payload, {
+      secret: process.env.REFRESH_TOKEN_SECRET,
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRATION,
+    });
+  }
+
+  async tokenValidateUser(payload: JwtPayload) {
+    return await this.userService.findOneById(payload.id);
   }
 }
