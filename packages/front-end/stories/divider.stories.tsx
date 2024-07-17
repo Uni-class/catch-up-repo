f1434f0557525bbf360d8335f94c24843b454bbc
nayounsang <nayounsang722@gmail.com>
2024-07-17 15:27:54 +0900

    feat: improve divider story template 세로 가로 구분선을 보기위한 템플릿 수정

--- packages/front-end/stories/divider.stories.tsx
+++ packages/front-end/stories/divider.stories.tsx
@@ -13,15 +13,21 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-const Template: StoryFn = (args) => {
+const Template: StoryFn = (_args) => {
   return (
-    <div>
-      <div>
+    <div
+      className={css({
+        display: "flex",
+        width: "50%",
+        height: "50%",
+      })}
+    >
+      <div className={css({ flex: 1 })}>
         <p>안녕하세요</p>
         <Divider />
         <p>반갑습니다</p>
       </div>
-      <div className={css({ display: "flex" })}>
+      <div className={css({ flex: 1, display: "flex" })}>
         <p>안녕하세요</p>
         <Divider direction="vertical" />
         <p>반갑습니다</p>
