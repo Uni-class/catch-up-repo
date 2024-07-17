b45af51b7694413820a198344b08fd08021e37e0
1000chw <1000chw@gmail.com>
2024-07-17 16:27:41 +0900

    feat: CreateUserDto 수정

--- packages/api-server/src/users/dto/create-user.dto.ts
+++ packages/api-server/src/users/dto/create-user.dto.ts
@@ -1,4 +1,36 @@
-import { PartialType } from '@nestjs/swagger';
-import { User } from '../entities/user.entity';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
 
-export class CreateUserDto extends PartialType(User) {}
+export class CreateUserDto {
+  @ApiProperty()
+  @IsString()
+  nickname: string;
+
+  @ApiProperty()
+  @IsString()
+  username: string;
+
+  @ApiProperty()
+  @IsString()
+  email: string;
+
+  @ApiProperty()
+  @IsString()
+  profileUrl: string;
+
+  @ApiProperty()
+  @IsString()
+  provider: string;
+
+  @ApiProperty()
+  @IsString()
+  providerId: string;
+
+  @ApiProperty()
+  @IsString()
+  refreshToken: string;
+
+  @ApiProperty()
+  @IsString()
+  status: string;
+}
