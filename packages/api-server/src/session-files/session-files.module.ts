ff5127c9e7ad91485a0f10d51b2239ecdf66e072
1000chw <1000chw@gmail.com>
2024-07-18 16:35:47 +0900

    feat: add session-file to feature

--- packages/api-server/src/session-files/session-files.module.ts
+++ packages/api-server/src/session-files/session-files.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SessionFile } from './entities/session-file.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([SessionFile])],
+})
 export class SessionFilesModule {}
