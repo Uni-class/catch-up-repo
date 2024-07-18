65ad125342f4677b115cb6626f446211331cd6fc
1000chw <1000chw@gmail.com>
2024-07-18 16:36:19 +0900

    feat: add user-file entity to feature

--- packages/api-server/src/user-files/user-files.module.ts
+++ packages/api-server/src/user-files/user-files.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserFile } from './entities/user-file.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([UserFile])],
+})
 export class UserFilesModule {}
