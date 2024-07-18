180fe10c41bca10699262e3a332ca6847b485208
nayounsang <nayounsang722@gmail.com>
2024-07-18 14:47:36 +0900

    rename: rename story stories도 네임 컨벤션에 맞게 변경

---
+++ packages/front-end/components/Text.tsx
@@ -0,0 +1,41 @@
+import { css, Styles } from "@/styled-system/css";
+import { styled } from "@/styled-system/jsx";
+
+const subStyle: Styles = {
+  opacity: 0.75,
+};
+
+export const Paragraph = styled("p", {
+  variants: {
+    variant: {
+      body1: {
+        fontSize: "1.5rem",
+      },
+      body2: {
+        fontSize: "1.25rem",
+      },
+      body3: {
+        fontSize: "1rem",
+      },
+      body4: {
+        fontSize: "0.75rem",
+      },
+      sub1: {
+        fontSize: "1.25rem",
+        ...subStyle,
+      },
+      sub2: {
+        fontSize: "1rem",
+        ...subStyle,
+      },
+      sub3: {
+        fontSize: "0.75rem",
+        ...subStyle,
+      },
+      sub4: {
+        fontSize: "0.5rem",
+        ...subStyle,
+      },
+    },
+  },
+});
