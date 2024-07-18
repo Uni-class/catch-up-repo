23dcc6e8b0c6cd9a28b4ce6c4ae2e1d67fc66dfc
nayounsang <nayounsang722@gmail.com>
2024-07-18 15:19:17 +0900

    feat: heading text 제목 텍스트를 구현

--- packages/front-end/components/Text.tsx
+++ packages/front-end/components/Text.tsx
@@ -38,7 +38,38 @@ export const Paragraph = styled("p", {
       },
     },
   },
-  defaultVariants:{
+  defaultVariants: {
     variant: "body1",
+  },
+});
+
+export const Heading = styled("h1", {
+  base:{
+    fontWeight:750,
+  },
+  variants: {
+    variant: {
+      h1: {
+        fontSize: "3.5rem",
+      },
+      h2: {
+        fontSize: "3rem",
+      },
+      h3: {
+        fontSize: "2.5rem",
+      },
+      h4: {
+        fontSize: "2rem",
+      },
+      h5: {
+        fontSize: "1.75rem",
+      },
+      h6: {
+        fontSize: "1.5rem",
+      },
+    },
+  },
+  defaultVariants:{
+    variant:"h1",
   }
 });
