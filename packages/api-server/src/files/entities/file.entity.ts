bd5ea7b53eb0f4d961a10f845338862da71a1bb5
1000chw <1000chw@gmail.com>
2024-07-18 14:37:22 +0900

    feat: Update file entity

--- packages/api-server/src/files/entities/file.entity.ts
+++ packages/api-server/src/files/entities/file.entity.ts
@@ -1,1 +1,43 @@
-export class File {}
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsDate, IsNumber, IsString } from 'class-validator';
+
+@Entity()
+export class File {
+  @ApiProperty()
+  @IsNumber()
+  @PrimaryGeneratedColumn()
+  fileId: number;
+
+  @ApiProperty()
+  @IsString()
+  @Column()
+  name: string;
+
+  @ApiProperty()
+  @IsString()
+  @Column()
+  url: string;
+
+  @ApiProperty()
+  @IsDate()
+  @CreateDateColumn()
+  createdAt: string;
+
+  @ApiProperty()
+  @IsDate()
+  @UpdateDateColumn()
+  updatedAt: string;
+
+  @ApiProperty()
+  @IsDate()
+  @DeleteDateColumn()
+  deletedAt: string;
+}
