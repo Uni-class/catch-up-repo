e6b7336c748a5c007d9c44120f4de5e18d7d28ea
1000chw <1000chw@gmail.com>
2024-07-18 14:31:11 +0900

    feat: Update Session entity 정

--- packages/api-server/src/sessions/entities/session.entity.ts
+++ packages/api-server/src/sessions/entities/session.entity.ts
@@ -1,1 +1,43 @@
-export class Session {}
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { IsNumber, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+@Entity()
+export class Session {
+  @ApiProperty()
+  @IsNumber()
+  @PrimaryGeneratedColumn()
+  sessionId: number;
+
+  @ApiProperty()
+  @IsString()
+  @Column()
+  sessionName: string;
+
+  @ApiProperty()
+  @IsString()
+  @Column()
+  hostId: number;
+
+  @ApiProperty()
+  @IsString()
+  @CreateDateColumn()
+  createdAt: string;
+
+  @ApiProperty()
+  @IsString()
+  @UpdateDateColumn()
+  updatedAt: string;
+
+  @ApiProperty()
+  @IsString()
+  @DeleteDateColumn()
+  closedAt: string;
+}
