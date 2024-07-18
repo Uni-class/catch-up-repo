5c3fed45f7710eaa7dcbbe1fed425bed321b5a50
1000chw <1000chw@gmail.com>
2024-07-18 16:38:11 +0900

    feat: Add user-session-file entity to feature

--- packages/api-server/src/user-session-files/user-session-files.module.ts
+++ packages/api-server/src/user-session-files/user-session-files.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserSessionFile } from './entities/user-session-file.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([UserSessionFile])],
+})
 export class UserSessionFilesModule {}
