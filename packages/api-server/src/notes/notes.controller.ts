02063a4f66dc13c48e24859c84ac820826666c63
1000chw <1000chw@gmail.com>
2024-07-18 15:41:50 +0900

    remove: Delete disuse function

--- packages/api-server/src/notes/notes.controller.ts
+++ packages/api-server/src/notes/notes.controller.ts
@@ -1,16 +1,17 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { NotesService } from './notes.service';
-import { CreateNoteDto } from './dto/create-note.dto';
-import { UpdateNoteDto } from './dto/update-note.dto';
 
 @Controller('notes')
 export class NotesController {
   constructor(private readonly notesService: NotesService) {}
-
-  @Post()
-  create(@Body() createNoteDto: CreateNoteDto) {
-    return this.notesService.create(createNoteDto);
-  }
 
   @Get()
   findAll() {
@@ -20,11 +21,6 @@ export class NotesController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.notesService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateNoteDto: UpdateNoteDto) {
-    return this.notesService.update(+id, updateNoteDto);
   }
 
   @Delete(':id')
