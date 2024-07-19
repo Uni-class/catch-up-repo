a339f8568b6b22c369c34877addbfdaebbdb225b
nayounsang <nayounsang722@gmail.com>
2024-07-19 14:39:59 +0900

    design: side bar user 유저 이름부분 클릭 및 호버링 가능하게

--- packages/front-end/app/dashboard/_components/Sidebar/SidebarUser.tsx
+++ packages/front-end/app/dashboard/_components/Sidebar/SidebarUser.tsx
@@ -3,22 +3,34 @@ import { css } from "@/styled-system/css";
 import SidebarUserSwitch from "./SidebarUserSwitch";
 
 export default function SidebarUser() {
-
   return (
     <div>
       <Paragraph variant="sub3" className={css({ margin: "8px 0" })}>
-        유저
+        역할 전환
       </Paragraph>
       <div
         className={css({
           display: "flex",
           alignItems: "justify-between",
+          marginBottom: "3rem",
         })}
       >
         <SidebarUserSwitch />
       </div>
-      <div className={css({ padding: "1rem 0" })}>
-        <p>유저이름</p>
+      <Paragraph variant="sub3" className={css({ margin: "8px 0" })}>
+        내 정보
+      </Paragraph>
+      <div
+        className={css({
+          padding: "1rem 0.5rem",
+          borderRadius: "8px",
+          cursor:"pointer",
+          "&:hover": {
+            bg: "gray.200",
+          },
+        })}
+      >
+        <p>아이콘, 유저이름</p>
       </div>
     </div>
   );
