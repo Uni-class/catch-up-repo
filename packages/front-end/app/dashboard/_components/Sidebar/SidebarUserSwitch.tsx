7fb2b34f8fe6f27e64f783188b78ca3833adb8d6
nayounsang <nayounsang722@gmail.com>
2024-07-19 15:57:24 +0900

    refactor: replace router with custom router in user-switch 사이드바유저스위치의 로직을 라우터커스텀훅을 이용해 가독성을 높임

--- packages/front-end/app/dashboard/_components/Sidebar/SidebarUserSwitch.tsx
+++ packages/front-end/app/dashboard/_components/Sidebar/SidebarUserSwitch.tsx
@@ -1,38 +1,20 @@
 import Switch from "@/components/Switch";
+import { useRouter } from "@/hook/useRouter";
 import { css } from "@/styled-system/css";
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { ChangeEvent, useState } from "react";
-
-function parsingQueryIter(query: IterableIterator<[string, string]>) {
-  const queryObj: { [key: string]: string } = {};
-  for (const [key, value] of query) {
-    queryObj[key] = value;
-  }
-  return queryObj;
-}
-
-function parsingQueryObj(pathname: string, query: { [key: string]: string }) {
-  // just construct dummy base url
-  const url = new URL(pathname, "http://example.com");
-  Object.keys(query).forEach((key) => {
-    url.searchParams.append(key, query[key]);
-  });
-  return `${url.pathname}${url.search}`;
-}
 
 export default function SidebarUserSwitch() {
   const router = useRouter();
-  const pathname = usePathname();
-  const queryParams = useSearchParams();
+
   const [isChecked, setIsChecked] = useState(
-    queryParams.get("role") === "tutor"
+    router.query.get("role") === "tutor"
   );
   const handleSwitchChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const queryObj = parsingQueryIter(queryParams.entries());
-    const newRole = queryParams.get("role") === "tutor" ? "student" : "tutor";
-    queryObj["role"] = newRole;
-    const href = parsingQueryObj(pathname, queryObj);
-    router.replace(href, { scroll: false });
+    const newRole = router.query.get("role") === "tutor" ? "student" : "tutor";
+    router.queryObj["role"] = newRole;
+    router.replace(router.getURLString(router.pathname, router.queryObj), {
+      scroll: false,
+    });
     setIsChecked(e.target.checked);
   };
   return (
