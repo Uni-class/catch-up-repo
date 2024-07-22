f03b19d955d78ccd97c5ce0b11d222454fbbabfe
nayounsang <nayounsang722@gmail.com>
2024-07-22 17:28:56 +0900

    feat: control size and align in storybook table 스토리북 table에 크기와 정렬을 제어할 수 있게함

--- packages/front-end/stories/Table.stories.tsx
+++ packages/front-end/stories/Table.stories.tsx
@@ -11,7 +11,26 @@ import { Meta, StoryFn, StoryObj } from "@storybook/react";
 const meta = {
   title: "Components/Table",
   component: TableContainer,
-} satisfies Meta<typeof TableContainer>;
+  argTypes: {
+    align: {
+      control: {
+        type: "radio",
+      },
+      options: ["left", "center", "right"],
+    },
+    size: {
+      control: {
+        type: "radio",
+      },
+      options: ["sm", "md", "lg"],
+    },
+  },
+} satisfies Meta<
+  React.ComponentProps<typeof TableContainer> & {
+    align?: "left" | "center" | "right";
+    size?: "sm" | "md" | "lg";
+  }
+>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
@@ -23,24 +42,30 @@ const bodyData: { name: string; location: string; tel: string }[] = [
   { name: "Aru", location: "JAP", tel: "010-9876-5432" },
 ];
 
-const Template: StoryFn = (_args) => {
+const Template: StoryFn = (args) => {
   return (
     <TableContainer>
       <TableHead>
         <TableRow>
-            <Th><input type="checkbox"/></Th>
+          <Th align={args.align}>
+            <input type="checkbox" />
+          </Th>
           {headData.map((e, i) => (
-            <Th key={i}>{e}</Th>
+            <Th align={args.align} key={i}>
+              {e}
+            </Th>
           ))}
         </TableRow>
       </TableHead>
       <TableBody>
         {bodyData.map((e, i) => (
           <TableRow key={i}>
-            <Td><input type="checkbox"/></Td>
-            <Td>{e.name}</Td>
-            <Td>{e.location}</Td>
-            <Td>{e.tel}</Td>
+            <Td {...args}>
+              <input type="checkbox" />
+            </Td>
+            <Td {...args}>{e.name}</Td>
+            <Td {...args}>{e.location}</Td>
+            <Td {...args}>{e.tel}</Td>
           </TableRow>
         ))}
       </TableBody>
