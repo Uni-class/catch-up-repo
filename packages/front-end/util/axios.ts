5f096b225d0927e57b45db55824cb0d0a7019401
nayounsang <nayounsang722@gmail.com>
2024-07-23 15:02:59 +0900

    feat: api scenario base url을 설정하고, 권한에러시 발생하는 시나리오 작성

--- packages/front-end/util/axios.ts
+++ packages/front-end/util/axios.ts
@@ -1,4 +1,5 @@
-import axios from "axios";
+import axios, { InternalAxiosRequestConfig } from "axios";
+import Cookies from "js-cookie";
 
 export const apiClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_SERVER,
@@ -7,3 +8,37 @@ export const apiClient = axios.create({
 export const apiAuthClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_SERVER,
 });
+
+apiAuthClient.interceptors.request.use(
+  (config) => {
+    const accessToken = Cookies.get("access_token");
+    config.headers.Authorization = `Bearer ${accessToken}`;
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+apiAuthClient.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    if (error.response.status === 403) {
+      const config = error.config as InternalAxiosRequestConfig<any>;
+      const refreshToken = Cookies.get("refresh_token");
+      try {
+        await apiClient.get("/token/refresh", {
+          headers: {
+            Authorization: `Bearer ${refreshToken}`,
+          },
+        });
+        return apiAuthClient(config);
+      } catch (e) {
+        return Promise.reject(e);
+      }
+    }
+    return Promise.reject(error);
+  }
+);
