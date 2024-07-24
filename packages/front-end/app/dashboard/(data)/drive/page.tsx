898f7146dd24a7cba04cff9a2169ca34cb7748f4
nayounsang <nayounsang722@gmail.com>
2024-07-24 17:23:38 +0900

    feat: add suspense and errorboundary in file table 파일테이블 비동기 상태 관리

--- packages/front-end/app/dashboard/(data)/drive/page.tsx
+++ packages/front-end/app/dashboard/(data)/drive/page.tsx
@@ -1,12 +1,17 @@
 "use client";
 
+import { Suspense } from "react";
 import FileTable from "./_components/FileTable";
+import { ErrorBoundary } from "react-error-boundary";
 
 export default function Page() {
   return (
     <>
-    
-      <FileTable />
+      <Suspense fallback={<h1>로딩...</h1>}>
+        <ErrorBoundary fallback={<h1>에러</h1>}>
+          <FileTable />
+        </ErrorBoundary>
+      </Suspense>
     </>
   );
 }
