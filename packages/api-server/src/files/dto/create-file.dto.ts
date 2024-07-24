09252d7f5d23e8fdd1e8ccc84b1dc2f7460d8913
1000chw <1000chw@gmail.com>
2024-07-24 18:25:44 +0900

    feat: file create dto

--- packages/api-server/src/files/dto/create-file.dto.ts
+++ packages/api-server/src/files/dto/create-file.dto.ts
@@ -1,1 +1,22 @@
-export class CreateFileDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { Column } from 'typeorm';
+
+export class CreateFileDto {
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  ownerId: number;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  @Column()
+  name: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  @Column()
+  url: string;
+}
