07df01959d169b878b7466e6b19af6d61b9b3fe7
nayounsang <nayounsang722@gmail.com>
2024-07-29 14:20:56 +0900

    fix: use useQuery instead of useSuspenseQuery server-side fetching으로 인한 오류방지를 위해 사용하는 hook 변경

--- packages/front-end/app/dashboard/(data)/session/host/page.tsx
+++ packages/front-end/app/dashboard/(data)/session/host/page.tsx
@@ -1,19 +1,16 @@
 "use client";
 
-import { Suspense } from "react";
-import SessionTable from "./_components/SessionTable";
 import { ErrorBoundary } from "react-error-boundary";
 import SessionHeader from "./_components/SessionHeader";
+import SessionTableFetch from "./_components/SessionTable";
 
 export default function Page() {
   return (
     <>
-     <SessionHeader/>
-      <Suspense fallback={<h1>로딩...</h1>}>
-        <ErrorBoundary fallback={<h1>에러</h1>}>
-          <SessionTable />
-        </ErrorBoundary>
-      </Suspense>
+      <SessionHeader />
+      <ErrorBoundary fallback={<h1>에러</h1>}>
+        <SessionTableFetch />
+      </ErrorBoundary>
     </>
   );
 }
