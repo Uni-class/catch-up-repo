941aa8aa2c48223d7658cca3a68a20b07dbfafd3
nayounsang <nayounsang722@gmail.com>
2024-07-29 17:44:51 +0900

    fix: drive table data fetch 드라이브 테이블에서 데이터 형식이 맞지않아 ui가 안뜨는 오류 해결

--- packages/front-end/app/files/page.tsx
+++ packages/front-end/app/files/page.tsx
@@ -1,17 +1,14 @@
 "use client";
 
-import { Suspense } from "react";
-import FileTable from "./_components/FileTable";
+import FileTableFetch from "./_components/FileTable";
 import { ErrorBoundary } from "react-error-boundary";
 
 export default function Page() {
   return (
     <>
-      <Suspense fallback={<h1>로딩...</h1>}>
-        <ErrorBoundary fallback={<h1>에러</h1>}>
-          <FileTable />
-        </ErrorBoundary>
-      </Suspense>
+      <ErrorBoundary fallback={<h1>에러</h1>}>
+        <FileTableFetch />
+      </ErrorBoundary>
     </>
   );
 }
