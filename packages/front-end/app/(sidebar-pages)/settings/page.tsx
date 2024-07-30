968d2178d152b9e4d27fcd658f09021c789c8d52
easyandones <easyandones@gmail.com>
2024-07-30 16:19:29 +0900

    feat: account settings page

--- packages/front-end/app/(sidebar-pages)/settings/page.tsx
+++ packages/front-end/app/(sidebar-pages)/settings/page.tsx
@@ -3,8 +3,8 @@
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 import Divider from "@/components/Divider";
-import {Heading} from "@/components/Text";
-import UserInfoEdit from "./_components/UserInfoEdit";
+import { Heading } from "@/components/Text";
+import UserProfileSettings from "./_components/UserProfileSettings";
 
 export default function Page() {
   return (
@@ -14,7 +14,7 @@ export default function Page() {
       <div>
         <ErrorBoundary fallback={<h1>예기치 못한 오류가 발생하였습니다. [임시 컴포넌트]</h1>}>
           <Suspense fallback={<h1>불러오는 중... [임시 컴포넌트]</h1>}>
-            <UserInfoEdit/>
+            <UserProfileSettings/>
           </Suspense>
         </ErrorBoundary>
       </div>
