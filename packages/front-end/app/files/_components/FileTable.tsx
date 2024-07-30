b198264703092f3d389dd76c9c851ce6e79782a9
nayounsang <nayounsang722@gmail.com>
2024-07-30 14:04:26 +0900

    fix: add key prop at file table row file table row에 key prop이 없어 추가

--- packages/front-end/app/files/_components/FileTable.tsx
+++ packages/front-end/app/files/_components/FileTable.tsx
@@ -56,6 +56,7 @@ export function FileTable({ data }: { data: File[] }) {
       <TableBody>
         {data.map((e) => (
           <Row
+          key={e.fileId}
             el={e}
             isCheckedOne={isCheckedOne}
             setIsCheckedOne={setIsCheckedOne}
