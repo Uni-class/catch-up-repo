1e7d222080afb9d70aa411961a7d2aa69a5f5336
nayounsang <nayounsang722@gmail.com>
2024-07-30 16:45:44 +0900

    refactor: file upload and select modal is union component 파일업로드및 선택 모달이 공용컴포넌트화

--- packages/front-end/app/(sidebar-pages)/sessions/create/page.tsx
+++ packages/front-end/app/(sidebar-pages)/sessions/create/page.tsx
@@ -1,16 +1,51 @@
 "use client";
 
 
-import { Heading } from "@/components/Text";
+import { Heading, Paragraph } from "@/components/Text";
 import Divider from "@/components/Divider";
+import Button from "@/components/Button";
+import { overlay } from "overlay-kit";
+import ModalContainer from "@/components/ModalContainer";
+import FileUploadAndSelectModal from "@/components/FileUploadAndSelectModal";
 
 
 export default function Page() {
+  const handleFileButtonClick = () => {
+    overlay.open(
+      ({ isOpen, close }) => (
+        <ModalContainer isOpen={isOpen} onClose={close}>
+          <FileUploadAndSelectModal />
+        </ModalContainer>
+      ),
+      { overlayId: "file upload" }
+    );
+  };
   return (
-    <div>
-      <Heading>세션 만들기</Heading>
+    <>
+      <Heading>세션 생성</Heading>
       <Divider/>
-      <div>세션 생성 페이지</div>
-    </div>
+      <form>
+      <label htmlFor="session title">세션 제목</label>
+        <input
+          id="session title"
+          name="session title"
+          placeholder="세션 제목 입력"
+  
+        />
+        <label htmlFor="select file">강의 자료 선택</label>
+        <Button
+          id="select file"
+          name="select file"
+          type="button"
+          onClick={handleFileButtonClick}
+        >
+          자료 선택
+        </Button>
+        <label>현재 선택한 파일</label>
+        <Paragraph>파일 제목 목록</Paragraph>
+        <Button type="submit">세션 시작</Button>
+      </form>
+
+    </>
   );
 }
