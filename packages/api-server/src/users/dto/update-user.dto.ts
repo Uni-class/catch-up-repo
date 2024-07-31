e010cb7d03cdb87ca5d467110f99addb574e0d39
1000chw <1000chw@gmail.com>
2024-07-31 17:14:38 +0900

    fix: update-user-dto revert

--- packages/api-server/src/users/dto/update-user.dto.ts
+++ packages/api-server/src/users/dto/update-user.dto.ts
@@ -1,12 +1,10 @@
 import { ApiProperty, PartialType, PickType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
-import {IsNumber, IsOptional, IsString} from 'class-validator';
+import { IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class UpdateUserDto extends PickType(PartialType(CreateUserDto), [
   'nickname',
   'email',
-]) {
-  @IsOptional()
-  @IsString()
-  profileUrl: string;
-}
+  'profileUrl',
+  'refreshToken',
+]) {}
