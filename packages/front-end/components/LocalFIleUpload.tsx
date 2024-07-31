970ed5013962e677ffff9a2d630a2293047b7bf3
nayounsang <nayounsang722@gmail.com>
2024-07-31 17:36:36 +0900

    feat: status text at file upload modal 파일존재별로 상태텍스트

--- packages/front-end/components/LocalFIleUpload.tsx
+++ packages/front-end/components/LocalFIleUpload.tsx
@@ -1,5 +1,7 @@
 import { Paragraph } from "@/components/Text";
 import { css } from "@/styled-system/css";
+import { format } from "date-fns";
+import { useState } from "react";
 import { DropEvent, FileRejection, useDropzone } from "react-dropzone";
 
 interface PropType {
@@ -15,10 +17,14 @@ export default function LocalFileUpload({
     console.log(acceptedFiles);
   },
 }: PropType) {
+  const [currentFile, setCurrentFile] = useState<File | null>(null);
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
-    onDrop: onDrop,
-    accept:{
-      'application/pdf': ['.pdf']
+    onDrop: (acceptedFiles, fileRejections, event) => {
+      onDrop(acceptedFiles, fileRejections, event);
+      setCurrentFile(acceptedFiles[0]);
+    },
+    accept: {
+      "application/pdf": [".pdf"],
     },
   });
   return (
@@ -46,12 +52,39 @@ export default function LocalFileUpload({
         })}
       >
         <input {...getInputProps()} />
-        {isDragActive ? (
-          <Paragraph variant="body1">이곳에 드래그 & 드랍</Paragraph>
-        ) : (
-          <Paragraph variant="body1">내 장치에서 업로드</Paragraph>
-        )}
+        <StatusText isDragActive={isDragActive} currentFile={currentFile} />
       </div>
     </div>
   );
 }
+
+function StatusText({
+  isDragActive,
+  currentFile,
+}: {
+  isDragActive: boolean;
+  currentFile: File | null;
+}) {
+  if (currentFile !== null) {
+    const lastModified = new Date(currentFile.lastModified);
+    return (
+      <div>
+        <Paragraph variant="body3">{currentFile.name}</Paragraph>
+        <Paragraph variant="body4">{`파일 용량: ${formatFileSize(currentFile.size)}`}</Paragraph>
+        <Paragraph variant="body4">{`최종 수정: ${format(lastModified, "yyyy-MM-dd")}`}</Paragraph>
+        <Paragraph variant="body2">눌러서 다시 선택</Paragraph>
+      </div>
+    );
+  }
+}
+
+const formatFileSize = (size: number) => {
+  if (size < 1024) return `${size} bytes`;
+  const units = ["KB", "MB", "GB", "TB"];
+  let unitIndex = -1;
+  do {
+    size /= 1024;
+    unitIndex++;
+  } while (size >= 1024 && unitIndex < units.length - 1);
+  return `${size.toFixed(2)} ${units[unitIndex]}`;
+};
