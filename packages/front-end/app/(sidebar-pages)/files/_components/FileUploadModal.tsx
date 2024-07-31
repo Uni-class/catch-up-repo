4dc19843aee0a46fc21516a4dbe35cb458928451
easyandones <easyandones@gmail.com>
2024-08-01 00:51:02 +0900

    refactor: file upload modal

--- packages/front-end/app/(sidebar-pages)/files/_components/FileUploadModal.tsx
+++ packages/front-end/app/(sidebar-pages)/files/_components/FileUploadModal.tsx
@@ -3,30 +3,41 @@ import FileUploader from "@/components/FileUploader/FileUploader";
 import { Heading } from "@/components/Text";
 import { css } from "@/styled-system/css";
 import { overlay } from "overlay-kit";
+import CloseIcon from "@/public/icons/close.svg";
 
 export default function FileUploadModal() {
   return (
     <div
       className={css({
-        width: "70vw",
-        height: "60vh",
-        backgroundColor: "#fff",
-        borderRadius: "1rem",
-        padding: "1rem",
         display: "flex",
+        padding: "1.5em 2em",
+        width: "70vw",
+        maxWidth: "50em",
+        height: "60vh",
+        maxHeight: "50em",
+        backgroundColor: "#ffffff",
+        borderRadius: "1em",
         flexDirection: "column",
+        gap: "1em",
       })}
     >
       <div
-        className={css({ display: "flex", justifyContent: "space-between" })}
+        className={css({
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        })}
       >
         <Heading>파일 업로드</Heading>
         <Button
+          className={css({
+            padding: "0.1em",
+          })}
           onClick={() => {
             overlay.close("File Upload");
           }}
         >
-          X
+          <CloseIcon width={"2em"} />
         </Button>
       </div>
       <FileUploader
