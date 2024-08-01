d98bded8873a28f82a45fab06cb349a16883b619
nayounsang <nayounsang722@gmail.com>
2024-08-01 16:56:19 +0900

    feat: set input for formData name을 설정

--- packages/front-end/app/(sidebar-pages)/settings/_components/UserProfileEdit.tsx
+++ packages/front-end/app/(sidebar-pages)/settings/_components/UserProfileEdit.tsx
@@ -2,7 +2,7 @@ import Button from "@/components/Button";
 import { Paragraph } from "@/components/Text";
 import { css } from "@/styled-system/css";
 import Image from "next/image";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import LineEdit from "@/components/LineEdit";
 import { User } from "@/schema/backend.schema";
 
@@ -11,7 +11,8 @@ interface PropType {
 }
 
 export default function UserProfileEdit({ data }: PropType) {
-  const [displayName, setDisplayName] = useState(data.nickname);
+  const fileInputRef = useRef<null | HTMLInputElement>(null);
+  const formRef = useRef<null | HTMLFormElement>(null);
 
   return (
     <form
@@ -20,32 +21,49 @@ export default function UserProfileEdit({ data }: PropType) {
         flexDirection: "column",
         gap: "1em",
       })}
+      ref={formRef}
     >
       <div
         className={css({
           display: "flex",
-          flexDirection:"column",
+          flexDirection: "column",
           gap: "1em",
         })}
       >
-        <div className={css({display:"flex",gap:"1.5rem"})}>
+        <div className={css({ display: "flex", gap: "1.5rem" })}>
           <div>
             <Paragraph>프로필 사진</Paragraph>
-          <Image
-            src={data.profileUrl || "/icon/icon-google.svg"}
-            alt="프로필 사진"
-            width={32}
-            height={32}
-            className={css({ borderRadius: "50%" })}
-          />  
+            <Image
+              src={data.profileUrl || "/icon/icon-google.svg"}
+              alt="프로필 사진"
+              width={32}
+              height={32}
+              className={css({ borderRadius: "50%" })}
+            />
           </div>
-          <Button>수정하기</Button>
+          <input
+            type="file"
+            accept="image/*"
+            ref={fileInputRef}
+            name="profileImage"
+            hidden
+          />
+          <Button
+            onClick={() => {
+              if (fileInputRef.current !== null) {
+                fileInputRef.current.click();
+              }
+            }}
+          >
+            수정하기
+          </Button>
         </div>
         <div>
           <Paragraph>닉네임</Paragraph>
           <LineEdit
             placeholder="닉네임을 입력해 주세요."
-            onChange={(event) => setDisplayName(event.target.value)}
+            defaultValue={data.nickname}
+            name="nickname"
           />
         </div>
       </div>
