f594d4a20c471ed45b9e7edafd20d778c83abec2
nayounsang <nayounsang722@gmail.com>
2024-08-01 13:59:04 +0900

    fix: add pollyfill for window.promise.withResolvers pdf.js 워커를 불러오는 중 노드22버전호환 함수를 사용해 폴리필 사용:

--- packages/front-end/app/view/[sessionId]/page.tsx
+++ packages/front-end/app/view/[sessionId]/page.tsx
@@ -1,5 +1,13 @@
-"use client"
+"use client";
 
-export default function Page(){
-    return <></>
+import "./_util/pdfWorkerPolyfill";
+import { pdfjs } from "react-pdf";
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  "pdfjs-dist/build/pdf.worker.min.mjs",
+  import.meta.url
+).toString();
+
+export default function Page() {
+  return <>lkj</>;
 }
