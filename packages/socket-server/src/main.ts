6c007fb350e7c1f4cba2c4905bd928413cab4193
1000chw <1000chw@gmail.com>
2024-08-03 20:27:08 +0900

    feat: default response append

--- packages/socket-server/src/main.ts
+++ packages/socket-server/src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionFilter } from './exception/all-exception.filter';
 import * as process from 'node:process';
+import { ResponseInterceptor } from './interceptor/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new AllExceptionFilter());
+  app.useGlobalInterceptors(new ResponseInterceptor());
   await app.listen(process.env.PORT);
 }
 bootstrap();
