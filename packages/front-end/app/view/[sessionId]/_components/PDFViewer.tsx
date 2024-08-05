39ae12e10d6b0b6cdac706874bd8834a34d923ea
easyandones <easyandones@gmail.com>
2024-08-05 14:54:57 +0900

    refactor: pdf-viewer

--- packages/front-end/app/view/[sessionId]/_components/PDFViewer.tsx
+++ packages/front-end/app/view/[sessionId]/_components/PDFViewer.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { pdfjs } from "react-pdf";
 import { Document, Page } from "react-pdf";
+import { PDFDocumentProxy } from "pdfjs-dist/types/src/pdf";
 import "react-pdf/dist/Page/TextLayer.css";
 import "react-pdf/dist/Page/AnnotationLayer.css";
 
@@ -11,40 +12,39 @@ import "react-pdf/dist/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
 export default function PDFViewer() {
-  const [numPages, setNumPages] = useState<number | null>(null);
-  const [pageNumber, setPageNumber] = useState<number>(1);
+  const [pageCount, setPageCount] = useState<number | null>(null);
+  const [currentPageIndex, setCurrentPageIndex] = useState<number>(1);
+
+
   useEffect(() => {
-    if (numPages === null) return;
+    if (pageCount === null) return;
     const handleKeyDown = (event: KeyboardEvent) => {
       if (event.key === "ArrowRight") {
-        setPageNumber((pageNumber) =>
-          pageNumber < numPages ? pageNumber + 1 : pageNumber
-        );
+        setCurrentPageIndex((index) => Math.min(pageCount - 1, index + 1));
       } else if (event.key === "ArrowLeft") {
-        setPageNumber((pageNumber) =>
-          pageNumber - 1 > 0 ? pageNumber - 1 : pageNumber
-        );
+        setCurrentPageIndex((index) => Math.max(0, index - 1));
       }
     };
     window.addEventListener("keydown", handleKeyDown);
     return () => {
       window.removeEventListener("keydown", handleKeyDown);
     };
-  }, [numPages]);
+  }, [pageCount]);
 
-  function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
-    setNumPages(numPages);
+  const onDocumentLoadSuccess = (pdf: PDFDocumentProxy) => {
+    setPageCount(pdf.numPages);
   }
+
   return (
     <div>
       <Document
         file="https://catch-up-dev-s3.s3.ap-northeast-2.amazonaws.com/1722418206623-[9-2] Index.pdf"
         onLoadSuccess={onDocumentLoadSuccess}
       >
-        <Page pageNumber={pageNumber} />
+        <Page pageIndex={currentPageIndex} />
       </Document>
       <p>
-        Page {pageNumber} of {numPages}
+        Page {currentPageIndex} of {pageCount}
       </p>
     </div>
   );
