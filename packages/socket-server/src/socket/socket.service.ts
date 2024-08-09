8be541338c4222bcf2aa5454e7f96185ca5771a9
1000chw <1000chw@gmail.com>
2024-08-09 18:53:00 +0900

    feat: logic upadate

--- packages/socket-server/src/socket/socket.service.ts
+++ packages/socket-server/src/socket/socket.service.ts
@@ -1,9 +1,9 @@
-import { Injectable } from '@nestjs/common';
-import { Socket } from 'socket.io';
+import { Injectable, UseFilters } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
 import { JwtPayload } from '../auth/jwt.payload';
 import { WsException } from '@nestjs/websockets';
+import { WsExceptionFilter } from '../exception/ws-exception.filter';
 
 @Injectable()
 export class SocketService {
@@ -11,19 +11,23 @@ export class SocketService {
     private jwtService: JwtService,
     private configService: ConfigService,
   ) {}
-  async validateUser(socket: Socket): Promise<number> {
-    const accessToken = socket.handshake.query?.access_token.toString();
-    if (!accessToken) return 0;
+
+  async validateUser(socket: any): Promise<number> {
     try {
-      const payload: JwtPayload = await this.jwtService.verifyAsync(
-        accessToken,
-        {
+      const cookies = socket.request.headers.cookie;
+      if (!cookies) return 0;
+      const accessToken = JSON.parse(cookies).access_token;
+      if (!accessToken) return 0;
+      const userId = await this.jwtService
+        .verifyAsync(accessToken, {
           secret: this.configService.get<string>('ACCESS_TOKEN_SECRET'),
-        },
-      );
-      return payload.id;
+        })
+        .then((payload: JwtPayload) => {
+          return payload.id;
+        });
+      return userId;
     } catch (ex) {
-      throw new WsException(ex.message);
+      return 0;
     }
   }
 }
