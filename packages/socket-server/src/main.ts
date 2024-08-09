ab6d7d1a5370acfe0993898074613b1fbc731029
1000chw <1000chw@gmail.com>
2024-08-09 18:52:26 +0900

    refactor: global exception delete

--- packages/socket-server/src/main.ts
+++ packages/socket-server/src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { AllExceptionFilter } from './exception/all-exception.filter';
 import * as process from 'node:process';
 import { ResponseInterceptor } from './interceptor/response.interceptor';
 import cookieParser from 'cookie-parser';
@@ -8,7 +7,6 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
-  app.useGlobalFilters(new AllExceptionFilter());
   app.useGlobalInterceptors(new ResponseInterceptor());
   await app.listen(process.env.PORT);
 }
