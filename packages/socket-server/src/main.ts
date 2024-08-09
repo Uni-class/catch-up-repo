10b755ac7ece45da817ee72f7ae644f8e9384446
1000chw <1000chw@gmail.com>
2024-08-09 17:39:27 +0900

    chore: add cookie-parser

--- packages/socket-server/src/main.ts
+++ packages/socket-server/src/main.ts
@@ -3,9 +3,11 @@ import { AppModule } from './app.module';
 import { AllExceptionFilter } from './exception/all-exception.filter';
 import * as process from 'node:process';
 import { ResponseInterceptor } from './interceptor/response.interceptor';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
   app.useGlobalFilters(new AllExceptionFilter());
   app.useGlobalInterceptors(new ResponseInterceptor());
   await app.listen(process.env.PORT);
