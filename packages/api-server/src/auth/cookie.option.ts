e883f91a406b9ff12212a4eec0a71eee5d2077c5
1000chw <1000chw@gmail.com>
2024-08-14 15:24:20 +0900

    fix: cookie.option fix

--- packages/api-server/src/auth/cookie.option.ts
+++ packages/api-server/src/auth/cookie.option.ts
@@ -2,11 +2,11 @@ import { ConfigService } from '@nestjs/config';
 
 const configService = new ConfigService();
 export const CookieOptions: {
-  sameSite: 'strict';
+  sameSite: 'lax';
   httpOnly: boolean;
   domain: string;
 } = {
-  sameSite: 'strict',
+  sameSite: 'lax',
   httpOnly: true,
-  domain: '.' + configService.get<string>('CLIENT_DOMAIN'),
+  domain: '.' + configService.get<string>('CLIENT_DOMAIN').split('://')[1],
 };
