bedf4caf063957f601e1126ca62b3603d63b8488
1000chw <1000chw@gmail.com>
2024-08-26 19:45:48 +0900

    refactor: delete console log

--- packages/api-server/src/sessions/sessions.service.ts
+++ packages/api-server/src/sessions/sessions.service.ts
@@ -99,7 +99,6 @@ export class SessionsService {
   async makeSessionCode(sessionId: number) {
     const session: Session = await this.findOne(sessionId);
     const code: string = bcrypt.hashSync(session.sessionName + Date.now(), 10);
-    console.log(code);
     return code.slice(7, 13);
   }
 
