4d3adfea20a4ddb8733c1fdda236b124f6ef5971
1000chw <1000chw@gmail.com>
2024-08-28 15:50:02 +0900

    feat: RedisIoAdapter connect to app

--- packages/socket-server/src/main.ts
+++ packages/socket-server/src/main.ts
@@ -4,11 +4,13 @@ import * as process from 'node:process';
 import { ResponseInterceptor } from './interceptor/response.interceptor';
 import cookieParser from 'cookie-parser';
 import helmet from 'helmet';
+import { RedisIoAdapter } from './redis-io.adapter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: process.env.CORS_ORIGIN_SITE, credentials: true });
-  console.log(process.env.CORS_ORIGIN_SITE,"hello");
+  const redisIoAdapter = new RedisIoAdapter(app);
+  await redisIoAdapter.connectToRedis();
   app.use(helmet());
   app.use(cookieParser());
   app.useGlobalInterceptors(new ResponseInterceptor());
