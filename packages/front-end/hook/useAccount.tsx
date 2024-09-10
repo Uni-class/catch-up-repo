4e00e2cb09e77a9dddfa70ddb56c28affb7e9fee
easyandones <easyandones@gmail.com>
2024-09-10 23:30:08 +0900

    fix build

--- packages/front-end/hook/useAccount.tsx
+++ packages/front-end/hook/useAccount.tsx
@@ -1,6 +1,12 @@
 "use client";
 
-import { ReactNode, createContext, useContext, useCallback } from "react";
+import {
+  ReactNode,
+  createContext,
+  useContext,
+  useCallback,
+  Suspense,
+} from "react";
 import { useQuery } from "@tanstack/react-query";
 import { AxiosResponse } from "axios";
 import { User } from "@/schema/backend.schema";
@@ -49,17 +55,19 @@ export const AccountProvider = ({ children }: { children: ReactNode }) => {
   const data: User | null = response?.data || null;
 
   return (
-    <AccountContext.Provider
-      value={{
-        updateAccount: () => {}, //need fix
-        logout: () => {}, //need fix
-        getLoginURL: getLoginURL,
-        goToLogin: goToLogin,
-        isLoading: isLoading,
-        account: data,
-      }}
-    >
-      {children}
-    </AccountContext.Provider>
+    <Suspense>
+      <AccountContext.Provider
+        value={{
+          updateAccount: () => {}, //need fix
+          logout: () => {}, //need fix
+          getLoginURL: getLoginURL,
+          goToLogin: goToLogin,
+          isLoading: isLoading,
+          account: data,
+        }}
+      >
+        {children}
+      </AccountContext.Provider>
+    </Suspense>
   );
 };
