edf37b783269006fdf9c4ae32890833aad1af1af
nayounsang <nayounsang722@gmail.com>
2024-09-14 22:12:52 +0900

    feat: implement bi-map 양방향 맵 구현

---
+++ packages/socket-server/src/utils/bi-map.class.ts
@@ -0,0 +1,37 @@
+export class BiMap<L = any, R = any> {
+  private leftToRight: Map<L, R>;
+  private rightToLeft: Map<R, L>;
+  constructor() {
+    this.leftToRight = new Map<L, R>();
+    this.rightToLeft = new Map<R, L>();
+  }
+
+  set(left: L, right: R): void {
+    const prevRight = this.getByLeft(left);
+    const prevLeft = this.getByRight(right);
+    if (prevRight !== undefined) {
+      this.deleteByRight(prevRight);
+    }
+    if (prevLeft !== undefined) {
+      this.deleteByLeft(prevLeft);
+    }
+    this.leftToRight.set(left, right);
+    this.rightToLeft.set(right, left);
+  }
+  getByLeft(left: L): R | undefined {
+    return this.leftToRight.get(left);
+  }
+  getByRight(right: R): L | undefined {
+    return this.rightToLeft.get(right);
+  }
+  deleteByLeft(left: L): void {
+    const right = this.getByLeft(left);
+    this.leftToRight.delete(left);
+    this.rightToLeft.delete(right);
+  }
+  deleteByRight(right: R): void {
+    const left = this.getByRight(right);
+    this.rightToLeft.delete(right);
+    this.leftToRight.delete(left);
+  }
+}
