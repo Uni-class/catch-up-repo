1551061fc1470c04c8988f907f26e812629194b3
nayounsang <nayounsang722@gmail.com>
2024-09-15 20:36:02 +0900

    feat: implement one-to-many map

---
+++ packages/socket-server/src/socket/utils/one-to-many-map.class.spec.ts
@@ -0,0 +1,10 @@
+import { OneToManyMap } from './one-to-many-map.class';
+
+describe('OneToManyMap', () => {
+  let oneToManyMap: OneToManyMap<string, number>;
+
+  beforeEach(() => {
+    oneToManyMap = new OneToManyMap<string, number>();
+  });
+  
+});
