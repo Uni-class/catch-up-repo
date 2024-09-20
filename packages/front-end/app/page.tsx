6116d709ac9ff4e1aa8930bb976d7dd91c535fa8
easyandones <easyandones@gmail.com>
2024-09-20 17:34:34 +0900

    fix: temporal main page

--- packages/front-end/app/page.tsx
+++ packages/front-end/app/page.tsx
@@ -1,16 +1,58 @@
 "use client";
 
 import Link from "next/link";
+import { css } from "@/styled-system/css";
 
 export default function Page() {
+  const tempNavBarItems = [
+    {
+      name: "로그인",
+      href: "/login",
+    },
+    {
+      name: "대시보드",
+      href: "/dashboard",
+    },
+  ];
+
   return (
     <main>
-      <p>메인 페이지</p>
-      <div>
-        <Link href={"/login"}>Login</Link>
+      <div
+        className={css({
+          display: "flex",
+          gap: "1em",
+          backgroundColor: "orange.400",
+        })}
+      >
+        {tempNavBarItems.map((item, index) => (
+          <div
+            key={index}
+            className={css({
+              padding: "0.5em 1em",
+              backgroundColor: "gray.300",
+              fontSize: "1.5em",
+              cursor: "pointer",
+              _hoverNotActive: {
+                color: "#ffffff",
+                backgroundColor: "gray.700",
+              },
+              _active: {
+                color: "#ffffff",
+                backgroundColor: "red.400",
+              },
+            })}
+          >
+            <Link href={item.href}>{item.name}</Link>
+          </div>
+        ))}
       </div>
-      <div>
-        <Link href={"/dashboard"}>Dashboard</Link>
+      <div
+        className={css({
+          padding: "1em",
+          fontSize: "2em",
+        })}
+      >
+        메인 페이지
       </div>
     </main>
   );
