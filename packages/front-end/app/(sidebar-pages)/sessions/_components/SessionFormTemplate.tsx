3477b777cd132b2d266b297d53b33b8923fad119
nayounsang <nayounsang722@gmail.com>
2024-09-29 18:38:02 +0900

    feat: session form error

--- packages/front-end/app/(sidebar-pages)/sessions/_components/SessionFormTemplate.tsx
+++ packages/front-end/app/(sidebar-pages)/sessions/_components/SessionFormTemplate.tsx
@@ -11,6 +11,7 @@ import { SessionFormType } from "@/type/SessionFormType";
 import { UseMutationResult } from "@tanstack/react-query";
 import { AxiosResponse } from "axios";
 import { overlay } from "overlay-kit";
+import { ReactNode, useState } from "react";
 
 export function SessionFormTemplate({
   useFormDataResult,
@@ -24,7 +25,11 @@ export function SessionFormTemplate({
     unknown
   >;
 }) {
-  const { unControlledDataRef, controlledData,idRef } = useFormDataResult;
+  const { unControlledDataRef, controlledData, idRef } = useFormDataResult;
+  const [error, setError] = useState({
+    sessionName: false,
+    sessionFiles: false,
+  });
   const handleFileButtonClick = () => {
     overlay.open(
       ({ isOpen, close }) => (
@@ -50,45 +55,100 @@ export function SessionFormTemplate({
       })}
       onSubmit={(e) => {
         e.preventDefault();
+        const { sessionName, sessionFiles } = unControlledDataRef.current;
+        const submitError = { sessionFiles: false, sessionName: false };
+        if (!sessionName.trim()) {
+          submitError.sessionName = true;
+        }
+        if (sessionFiles.length === 0) {
+          submitError.sessionFiles = true;
+        }
+        if (submitError.sessionFiles || submitError.sessionName) {
+          setError({ ...submitError });
+          return;
+        }
         formMutation.mutate({
-          sessionName: unControlledDataRef.current.sessionName,
-          sessionFileIds: unControlledDataRef.current.sessionFiles.map(
-            (file) => file.fileId
-          ),
+          sessionName: sessionName,
+          sessionFileIds: sessionFiles.map((file) => file.fileId),
         });
       }}
     >
-      <Label htmlFor="session-title">세션 제목</Label>
-      <LineEdit
-        id="session-title"
-        name="session-title"
-        placeholder="세션 제목을 입력해주세요"
-        onChange={handleInputChange}
-        className={css({ height: "3em" })}
-        defaultValue={unControlledDataRef.current.sessionName}
-      />
-      <Label htmlFor="select-file">강의 자료 선택</Label>
-      <Button
-        id="select-file"
-        name="select-file"
-        type="button"
-        onClick={handleFileButtonClick}
-        className={css({
-          height: "3em",
-          backgroundColor: "green.600",
-        })}
+      <ControlContainer
+        labelText="세션 제목"
+        htmlFor="session-title"
+        errorText="세션 제목을 입력해주세요."
+        isError={error.sessionName}
       >
-        자료 선택
-      </Button>
-      <Label>현재 선택한 파일</Label>
-      <Paragraph>
-        {controlledData.sessionFiles.length > 0
-          ? controlledData.sessionFiles[0].name
-          : "현재 선택한 파일이 없습니다."}
-      </Paragraph>
+        <LineEdit
+          id="session-title"
+          name="session-title"
+          placeholder="세션 제목을 입력해주세요"
+          onChange={handleInputChange}
+          className={css({ height: "3em" })}
+          defaultValue={unControlledDataRef.current.sessionName}
+        />
+      </ControlContainer>
+      <ControlContainer
+        labelText="강의 자료 선택"
+        htmlFor="select-file"
+        errorText="세션 파일을 선택해주세요."
+        isError={error.sessionFiles}
+      >
+        <Button
+          id="select-file"
+          name="select-file"
+          type="button"
+          onClick={handleFileButtonClick}
+          className={css({
+            height: "3em",
+            backgroundColor: "green.600",
+          })}
+        >
+          자료 선택
+        </Button>
+      </ControlContainer>
+      <ControlContainer labelText="현재 선택한 파일">
+        <Paragraph>
+          {controlledData.sessionFiles.length > 0
+            ? controlledData.sessionFiles[0].name
+            : "현재 선택한 파일이 없습니다."}
+        </Paragraph>
+      </ControlContainer>
       <Button type="submit" className={css({ height: "3em" })}>
         세션 시작
       </Button>
     </form>
+  );
+}
+
+function ControlContainer({
+  children,
+  labelText,
+  htmlFor,
+  errorText,
+  isError,
+  height,
+}: {
+  children?: ReactNode;
+  labelText?: string;
+  htmlFor?: string;
+  errorText?: string;
+  isError?: boolean;
+  height?: React.CSSProperties["height"];
+}) {
+  return (
+    <div
+      className={css({
+        display: "flex",
+        flexDirection: "column",
+        gap: "0.75rem",
+        width: "100%",
+        height: height,
+      })}
+    >
+      <Label htmlFor={htmlFor}>{labelText}</Label>
+      {children}
+      {!!isError && <p className={css({ color: "red.500" })}>{errorText}</p>}
+    </div>
   );
 }
