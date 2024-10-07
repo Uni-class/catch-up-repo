44306439a089ee8918691e8cf0388bc2976fc231
1000chw <1000chw@gmail.com>
2024-10-07 16:12:56 +0900

    refactor: unused item delete

--- packages/api-server/src/users/schemas/note.schema.ts
+++ packages/api-server/src/users/schemas/note.schema.ts
@@ -1,7 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { HydratedDocument, Document } from 'mongoose';
-
-export type CatDocument = HydratedDocument<Note>;
+import { Document } from 'mongoose';
 
 @Schema()
 export class Note extends Document {
