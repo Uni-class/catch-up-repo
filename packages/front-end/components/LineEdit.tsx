d4e3f7c7f594738d44f0b00ded5aba0e17959ccf
nayounsang <nayounsang722@gmail.com>
2024-10-17 21:49:26 +0900

    refactor: remove comment and unused in lineedit

--- packages/front-end/components/LineEdit.tsx
+++ packages/front-end/components/LineEdit.tsx
@@ -1,41 +1,5 @@
-import { css, cx } from "@/styled-system/css";
 import { styled } from "@/styled-system/jsx";
-import { DetailedHTMLProps, InputHTMLAttributes } from "react";
 
-// interface PropType
-//   extends DetailedHTMLProps<
-//     InputHTMLAttributes<HTMLInputElement>,
-//     HTMLInputElement
-//   > {}
-
-// export default function LineEdit({ className, ...attr }: PropType) {
-//   return (
-//     <input
-//       {...attr}
-//       type="text"
-//       className={cx(
-//         className,
-//         css({
-//           padding: "0.5em 0.8em",
-//           color: "#000000",
-//           background: "#ededed",
-//           border: "2px solid transparent",
-//           borderRadius: "0.4em",
-//           outline: "none",
-//           transition: "all 0.15s ease-in-out",
-//           _hoverNotFocus: {
-//             borderColor: "orange.300",
-//             background: "#dcdcdc",
-//           },
-//           _focus: {
-//             borderColor: "red.400",
-//             background: "#eeeeee",
-//           },
-//         }),
-//       )}
-//     />
-//   );
-// }
 
 const LineEdit = styled("input", {
   base: {
