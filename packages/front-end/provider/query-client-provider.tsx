89405e6ba05f7dc52b4cd74e0946c7963486111d
nayounsang <nayounsang722@gmail.com>
2024-10-28 17:26:18 +0900

    refactor: only use useQUery

--- packages/front-end/provider/query-client-provider.tsx
+++ packages/front-end/provider/query-client-provider.tsx
@@ -1,11 +1,13 @@
 "use client";
 import {
+  QueryCache,
   QueryClient,
   QueryClientProvider as _QueryClientProvider,
 } from "@tanstack/react-query";
 import { ReactNode, useState } from "react";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { AxiosError } from "axios";
+import { refreshClient } from "@/utils/axios";
 
 interface PropType {
   children: ReactNode;
@@ -30,12 +32,23 @@ export default function QueryClientProvider({ children }: PropType) {
             shouldDehydrateQuery: (_query) => false,
           },
         },
+        queryCache: new QueryCache({
+          onError: (_error, query) => {
+            const error = _error as AxiosError;
+            if (error.response?.status === 401) {
+              refreshClient
+                .get("/auth/token-refresh")
+                .then(() => {
+                  queryClient.refetchQueries({ queryKey: query.queryKey }); // (query) ??
+                })
+            }
+          },
+        }),
       })
   );
   return (
     <_QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
-
       {children}
     </_QueryClientProvider>
   );
