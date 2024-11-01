aa4147518cfd9c6d7ef9e85ed51e2f421327db81
1000chw <1000chw@gmail.com>
2024-11-01 20:17:42 +0900

    feat: Make GetSessionsResponseDto

---
+++ packages/api-server/src/users/dto/get-sessions-response.dto.ts
@@ -0,0 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber } from 'class-validator';
+import { Session } from '../../sessions/entities/session.entity';
+
+export class GetSessionsResponseDto {
+  @ApiProperty()
+  @IsNumber()
+  totalPages: number;
+
+  @ApiProperty()
+  @IsNumber()
+  page: number;
+
+  @ApiProperty()
+  sessions: Session[];
+
+  constructor(totalPages: number, page: number, sessions: Session[]) {
+    this.totalPages = totalPages;
+    this.page = page;
+    this.sessions = sessions;
+  }
+}
