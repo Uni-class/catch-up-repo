85e402be0be49aa0495abcdc173673580f06501c
easyandones <easyandones@gmail.com>
2024-11-02 16:37:18 +0900

    feat: apply pagination (files api)

--- packages/front-end/app/(sidebar-pages)/files/_fetcher/FileTableFetcher.tsx
+++ packages/front-end/app/(sidebar-pages)/files/_fetcher/FileTableFetcher.tsx
@@ -2,28 +2,50 @@ import { useQuery } from "@tanstack/react-query";
 import { File } from "@/schema/backend.schema";
 import { apiClient } from "@/utils/axios";
 import { FileTable } from "../_components/FileTable";
-
+import { useState } from "react";
+import { AxiosResponse } from "axios";
 
 export default function FileTableFetcher() {
-  const { data: response, isLoading, isError } = useQuery({
-    queryKey: ["user", "files"],
+  const size = 5;
+
+  const [page, setPage] = useState(0);
+
+  const {
+    data: response,
+    isLoading,
+    isError,
+  } = useQuery<
+    AxiosResponse<{
+      page: number;
+      totalPages: number;
+      files: File[];
+    }>
+  >({
+    queryKey: ["user", "files", size, page],
     queryFn: async () => {
-      return await apiClient.get<File[]>("/user/files");
-    }
+      return await apiClient.get("/user/files", {
+        params: {
+          size: size,
+          page: page + 1,
+        },
+      });
+    },
   });
   const data = response?.data;
-  const status = (
-    isLoading
-    ?
-      "loading"
-      :
-      (
-        isError || !Array.isArray(data)
-        ?
-          "error"
-          :
-          null
-      )
+  const status = isLoading
+    ? "loading"
+    : isError || !Array.isArray(data)
+      ? "error"
+      : null;
+  return (
+    <FileTable
+      data={data ? { ...data, page: data.page - 1 } : undefined}
+      pagination={{
+        size: size,
+        index: page,
+        setIndex: setPage,
+      }}
+      status={status}
+    />
   );
-  return <FileTable data={data || []} status={status} />
 }
